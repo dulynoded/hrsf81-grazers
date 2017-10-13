79da39538b77b7e29a0ba0d01a5ab284d920f9ed
Claire Johnson <c.eliseee@gmail.com>
2017-10-12 19:10:08 -0700

    (feat) remove role option

--- client/src/components/createEvent.component.js
+++ client/src/components/createEvent.component.js
@@ -9,23 +9,17 @@ module.exports = {
 
     this.startDate = new Date();
     this.endDate = new Date();
-    this.roles = [];
     this.groups = [];
 
     $scope.form = {
       name: '',
-      role: '',
       group: '',
     };
 
+    // Add groups to staff role
     this.addGroup = () => {
       this.groups.push($scope.form.group);
       $scope.form.group = '';
-    };
-
-    this.addRole = () => {
-      this.roles.push($scope.form.role);
-      $scope.form.role = '';
     };
 
     this.handleClick = () => {
@@ -33,7 +27,6 @@ module.exports = {
       const obj = {
         start_date: this.startDate.toString(),
         end_date: this.endDate.toString(),
-        roles: this.roles,
         groups: this.groups,
         organizer_id: this.user.id,
       };
