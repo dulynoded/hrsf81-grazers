633533de87936cb2b7f0f62e9749256c124c913b
Clara Cho <clara.cho@gmail.com>
2017-10-13 19:32:24 -0700

    modify config variables

--- server/helpers/aws.js
+++ server/helpers/aws.js
@@ -1,20 +1,21 @@
 const AWS = require('aws-sdk');
-let secrets;
+let accessKey;
+let secretAccessKey;
 
 try {
-  secrets = require('../config/aws');
-  AWS.config.update({
-    accessKeyId: secrets.AWS_ACCESS_KEY_ID,
-    secretAccessKey: secrets.AWS_SECRET_ACCESS_KEY,
-    "region": "us-west-1"
-  });
+  const secrets = require('../config/aws');
+  accessKey = secrets.AWS_ACCESS_KEY_ID;
+  secretAccessKey = secrets.AWS_SECRET_ACCESS_KEY;
 } catch(err) {
-  AWS.config.update({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKey = process.env.ACCESS_KEY_ID;
+  secretAccessKey = process.env.SECRET_ACCESS_KEY;
+}
+
+AWS.config.update({
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
     "region": "us-west-1"
   });
-}
 
 const s3bucket = new AWS.S3({params: {Bucket: 'hrsf81-grazers'}, apiVersion: '2006-03-01' });
 s3bucket.listObjects().promise()
