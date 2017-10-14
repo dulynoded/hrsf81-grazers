0a731e4a7fbdbc96381cb237ec7b1918dc6d8a26
Claire Johnson <c.eliseee@gmail.com>
2017-10-14 16:21:17 -0700

    (cleanup) general cleanup

--- server/schedule.js
+++ server/schedule.js
@@ -13,69 +13,30 @@ router.route('/')
       .then(data => res.status(201).send(data.rows[0]));
   });
 
-// router.get('/:eventId', (req, res) => {
-//   const { eventId } = req.params;
-//   db.getSchedulesByEvent(eventId)
-//     .then((schedulesData) => {
-//       return Promise.all(schedulesData.rows.map((schedule) => {
-//         const { date } = schedule;
-//         return db.getActivitiesByDay(schedule.id)
-//           .then((activitiesData) => {
-//             return Promise.all(activitiesData.rows.map((actData) => {
-//               const activity = actData;
-//               return db.getGroupNamesByActivity(activity.id)
-//                 .then((groups) => {
-//                   // assign arbitrary date to time in order to parse in front end
-//                   activity.starttime = new Date(`1970-01-01T${activity.starttime}`);
-//                   activity.endtime = new Date(`1970-01-01T${activity.endtime}`);
-//                   activity.groups = groups.rows;
-//                   return activity;
-//                 });
-//             }));
-//           })
-//           .then((timetable) => {
-//             return { date, timetable };
-//           });
-//       }));
-//     })
-//     .then((results) => {
-//       res.status(200).send(results);
-//     });
-// });
-
-const getGroupsByActivityHelper = activity => (
-  db.getGroupNamesByActivity(activity.id)
-    .then((groups) => {
-      // assign arbitrary date to time in order to parse in front end
-      activity.starttime = new Date(`1970-01-01T${activity.starttime}`);
-      activity.endtime = new Date(`1970-01-01T${activity.endtime}`);
-      activity.groups = groups.rows;
-      return activity;
-    })
-);
-
 router.get('/:eventId', (req, res) => {
   const { eventId } = req.params;
-  let date;
   db.getSchedulesByEvent(eventId)
     .then((schedulesData) => {
-      const promiseArr = [];
-      schedulesData.rows.forEach((schedule) => {
-        date = schedule.date;
-        promiseArr.push(db.getActivitiesByDay(schedule.id)
+      return Promise.all(schedulesData.rows.map((schedule) => {
+        const { date } = schedule;
+        return db.getActivitiesByDay(schedule.id)
           .then((activitiesData) => {
-            const promiseArrActivities = [];
-            activitiesData.rows.forEach((actData) => {
+            return Promise.all(activitiesData.rows.map((actData) => {
               const activity = actData;
-              promiseArrActivities.push(getGroupsByActivityHelper(activity));
-            });
-            return Promise.all(promiseArrActivities);
+              return db.getGroupNamesByActivity(activity.id)
+                .then((groups) => {
+                  // assign arbitrary date to time in order to parse in front end
+                  activity.starttime = new Date(`1970-01-01T${activity.starttime}`);
+                  activity.endtime = new Date(`1970-01-01T${activity.endtime}`);
+                  activity.groups = groups.rows;
+                  return activity;
+                });
+            }));
           })
-          .then(timetable => (
-            { date, timetable }
-          )));
-      });
-      return Promise.all(promiseArr);
+          .then((timetable) => {
+            return { date, timetable };
+          });
+      }));
     })
     .then((results) => {
       res.status(200).send(results);
